8a9919d62f86294b007ac085c2ebf58f0db737eb
amalkaarikan <87614674+amalkaarikan@users.noreply.github.com>
2025-11-04 12:17:59 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    ibm = {
+      source  = "IBM-Cloud/ibm"
+      version = "~> 1.55.0"
+    }
+  }
+
+  required_version = ">= 1.3.0"
+}
+
+provider "ibm" {
+  ibmcloud_api_key = var.ibmcloud_api_key
+  region           = var.region
+}
+
+resource "ibm_resource_group" "rg_demo" {
+  name = var.resource_group_name
+}
+
+resource "ibm_resource_instance" "cos_instance" {
+  name              = "demo-cos-instance"
+  service           = "cloud-object-storage"
+  plan              = "standard"
+  location          = var.region
+  resource_group_id = ibm_resource_group.rg_demo.id
+}
+
+output "cos_instance_id" {
+  value = ibm_resource_instance.cos_instance.id
+}
