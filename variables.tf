02f96936092edce9a94fbbcd5c298442cf6ed030
amalkaarikan <87614674+amalkaarikan@users.noreply.github.com>
2025-11-05 00:23:42 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "ibmcloud_api_key" {
 variable "region" {
   description = "IBM Cloud region"
   type        = string
-  default     = ""global"
+  default     = "us-south" # or your preferred region
 }
 
 variable "resource_group_name" {
