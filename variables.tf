8e9bcba95f63223d6771742bdd228638479b976a
amalkaarikan <87614674+amalkaarikan@users.noreply.github.com>
2025-11-04 12:20:00 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "ibmcloud_api_key" {
+  description = "IBM Cloud API Key"
+  type        = string
+  sensitive   = true
+}
+
+variable "region" {
+  description = "IBM Cloud region"
+  type        = string
+  default     = "us-south"
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+  default     = "demo-terraform-rg"
+}
